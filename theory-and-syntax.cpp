76216ad75c4dae005760712d6fbf83627200d673
Manittecool1213 <57066306+Manittecool1213@users.noreply.github.com>
2023-11-21 18:34:26 +0530

    Began revising theory and syntax
    Started work on theory-and-syntax.cpp

---
+++ theory-and-syntax.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+// Inludes all common libraries for competitive programming.
+
+using namespace std;
+// Avoids need to write 'std::cout', etc. every single time.
+
+int main() {
+    cout << "Hello World!" << endl;
+    // endl: works just like /n; moves cursor to next line.
+
+    cout << "Example of " << "concatenation." << endl;
+
+    return 0;
+}
