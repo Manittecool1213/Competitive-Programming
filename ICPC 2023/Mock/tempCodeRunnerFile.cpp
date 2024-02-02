cc129204f3659f9df0ea0b49d5991f1ab22ab6f2
Manittecool1213 <57066306+Manittecool1213@users.noreply.github.com>
2024-02-02 18:34:57 +0530

    deleted .exe files

---
+++ ICPC 2023/Mock/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+    // for(auto i = trial_array.begin(); i != trial_array.end(); i++) {
+    //     cout << *i << endl;
+    // }
--- ICPC 2023/Mock/trial.cpp
+++ ICPC 2023/Mock/trial.cpp
@@ -1,32 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sample_function(int* input_array);
-
 int main()
 {
-    // for(int i = 0; i < 6; i++) {
-    //     for(int j = 0; j < 6; j++) {
-    //         cout << i << "^" << j << ": " << (i^j) << endl;
-    //     }
-    // }
-    // cout << (10^2) << endl;
-    // sample_function();
+    
 
-    // bool sample_boolean = true;
-    // cout << sample_boolean << endl;
-
-    // int sample_array[] = {1, 2, 3, 4, 4};
-    // int size = sizeof(sample_array) / sizeof(sample_array[0]);
-    // cout << is_sorted(sample_array, sample_array + size) << endl;
-
-    cout << (2^1) << endl;
     return 0;
-}
-
-void sample_function(int* input_array) {
-    cout << sizeof(* input_array)/sizeof(int) << endl;
-    // for(int i = 0; i < sizeof(input_array)/sizeof(int); i++) {
-    //     cout << i << endl;
-    // }
 }
